52a7bff26102539b2318924944fa8da1903add92
Emma Stoverink <emmajs16@live.com>
2016-12-07 18:32:10 +0000

    changed app controller

--- smart-kookies/application-controller.rb
+++ smart-kookies/application-controller.rb
@@ -8,7 +8,7 @@ class MyApp < Sinatra::Base
   erb :index
  end
 
- get '/questions.h' do
+ get '/questions' do
    erb :questions
  end
 
@@ -47,33 +47,5 @@ class MyApp < Sinatra::Base
  get '/index.html' do
    erb :index
  end
- # get '' do
- #   erb :breakfast
- # end
- #
- # get '/snack' do
- #   erb :snack
- # end
- #
- # get '/dessert' do
- #   erb :dessert
- # end
 
-#  post  '/results' do
-#    answers = params.values
-#    @total= 0
-#    answers.each do |answer|
-#      @total += answer.to_i
-#    end
-#    puts @total
-#
-#    @cookie = cookie_chooser(@total)
-#    if @cookie == "cranberry"
-#      erb :cranberry
-#    elsif @cookie == "trailmix"
-#      erb :trailmix
-#    elsif @cookie == "chocolatey"
-#      erb :chocolatey
-#   end
-# end
 end
